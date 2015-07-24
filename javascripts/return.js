27c8f950044c253f7db9acce8a3a8a78c86971a1
Spencer <1019777269@qq.com>
2015-07-24 23:23:02 +0800

    201507242322+fix-return

---
+++ javascripts/return.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#return_main").submit(function(){
+        var text_return = $(this).find("textarea").val();
+        if (!text_return) return false;
+        $.ajax({
+            type:"POST",
+            url:"",
+            async:false,
+            data:{"text_return":text_return}，
+            dataType:"json",
+            success:function(data){
+                if (data) {
+                    $(".box_bg").show();
+                    setTimeout(function(){
+                        location.href = "";
+                    },3000)
+                };
+            }
+        })
+    }) 
+})
