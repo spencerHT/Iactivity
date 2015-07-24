c429c23968ae1cb403bb9f0191c73b0616927be6
Spencer <1019777269@qq.com>
2015-07-24 10:29:08 +0800

    201507241021+login

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,13 +3,20 @@ $(function(){
     $(".change").click(function(){
         if (change_state == 0) {
             change_state++;
-            $(".login_main").eq(0).hide();
-            $(".login_main").eq(1).show();
+            $(".login_main").eq(0).addClass("hide");
+            $(".login_main").eq(1).removeClass("hide");
         }
         else {
             change_state--;
-            $(".login_main").eq(0).show();
-            $(".login_main").eq(1).hide();
+            $(".login_main").eq(0).removeClass("hide");
+            $(".login_main").eq(1).addClass("hide");
         }
+    })
+    $(".login_main").submit(function(){
+        var name = $(this).find("input[type='text']").val();
+        var pwd = $(this).find("input[type='password']").val();
+        if (!name || !pwd) {
+            return false;
+        };
     })
 })
